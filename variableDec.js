e1cec1fa4388e25c16c28b5e429f0af1b356d17f
NithyaSadasivam17 <nithismila@gmail.com>
2025-08-16 23:38:52 +0530

    Add files via upload
    Day1 Assignment

---
+++ variableDec.js
@@ -0,0 +1,19 @@
+const city = "Chennai"
+console.log("I live in "+city);
+console.log(typeof city)
+let marks = 85
+console.log(marks+10);
+console.log(typeof marks)
+var isWeekend =  true
+if(isWeekend)
+{
+    console.log("Relax");
+}
+else
+{
+    console.log("work")
+}
+console.log(typeof isWeekend)
+var futureGoal
+console.log(typeof futureGoal)
+
